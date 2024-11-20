5c9de4349f6bd51b6bb05ee58795d47bd9235a96
Naveen <naveenanand173@gmail.com>
2024-11-20 16:15:37 +0530

    cloudinary was updated

---
+++ server/src/middleware/auth.middleware.js
@@ -0,0 +1,32 @@
+import jwt from 'jsonwebtoken';
+import User from '../models/user.model.js';
+
+export const protectRoute = async (req, res) => {
+    try{
+        const token = req.cookies.jwt;
+
+        if(!token) {
+            return res.status(401).json({message: "Unauthorized-no token found"});
+        }
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        if(!decoded){
+            return res.status(401).json({message: "Unauthorized-token is invalid"});
+        }
+
+        const user = await User.findById(decoded.UserId).select("-password");
+
+        if(!user){
+            return res.status(401).json({message: "Unauthorized-user not found"});
+        }
+
+        req.user = user;
+
+        next()
+
+    } catch (error) {
+        console.log("Error in Auth Middlerware", error);
+        return res.status(500).json({message: "Internal Server Error"});
+    }
+}
