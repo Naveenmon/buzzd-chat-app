1b3b9d75e991a5e7dc1d8e2c7d02b9b638f09428
Naveen <naveenanand173@gmail.com>
2024-11-20 11:27:50 +0530

    database connected

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,17 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
 import authRoutes from './routes/auth.route.js';
+import { connectDB } from './lib/db.js';
 
 const app = express();
 
-app.arguments("/api/auth", authRoutes)
+dotenv.config
+const PORT = process.env.PORT || 5001;
+
+app.use("/api/auth", authRoutes)
 
 app.listen(5001, () => {
-    console.log('Server is running on port 5001');
+    console.log(`Server is running on port ${PORT} `);
+    connectDB();
 })
